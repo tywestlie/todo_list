6918639ce9a6a1e6fa2a597e9ecb08dc3a05a6c3
Tyler Westlie <tyler.westlie@gmail.com>
2019-01-29 18:06:06 -0700

    adds todo item

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,7 +1,7 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 
 
-class  extends React.Component {
+export class TodoItem extends React.Component {
   render () {
     return (
       <div>
