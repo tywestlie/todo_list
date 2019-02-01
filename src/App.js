73c3c99abd2743796fe5f18ed9dba8106cf0bd6e
Tyler Westlie <tyler.westlie@gmail.com>
2019-02-01 15:32:32 -0700

    adds more styling, adds header, adds addtodo field

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
+import Header from './components/layout/Header';
 import Todos from './components/Todos';
+import AddTodo from './components/AddTodo';
 
 class App extends Component {
   state = {
@@ -42,7 +44,11 @@ delTodo = (id) => {
 render() {
   return (
     <div className="App">
-      <Todos todos={this.state.todos} markComplete={this.markComplete} delTodo={this.delTodo}/>
+      <div className="container">
+        <Header />
+        <AddTodo />
+        <Todos todos={this.state.todos} markComplete={this.markComplete} delTodo={this.delTodo}/>
+      </div>
     </div>
   );
 }
