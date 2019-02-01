0cfcc75eb9b7b91cecc171ab6517d684f525db71
Tyler Westlie <tyler.westlie@gmail.com>
2019-02-01 15:58:16 -0700

    adds uuid to each todo

--- src/App.js
+++ src/App.js
@@ -2,22 +2,23 @@ import React, { Component } from 'react';
 import Header from './components/layout/Header';
 import Todos from './components/Todos';
 import AddTodo from './components/AddTodo';
+import uuid from 'uuid';
 
 class App extends Component {
   state = {
     todos: [
       {
-        id: 1,
+        id: uuid.v4(),
         title: 'Take Out The Trash',
         completed: false
       },
       {
-        id: 2,
+        id: uuid.v4(),
         title: 'Pet Cat',
         completed: false
       },
       {
-        id: 3,
+        id: uuid.v4(),
         title: 'Clean Kitchen',
         completed: false
       }
@@ -40,13 +41,23 @@ delTodo = (id) => {
   this.setState({ todos: [...this.state.todos.filter(todo => todo.id !== id)] });
 }
 
+// addTodo
+addTodo = (title) => {
+  const newTodo = {
+    id: uuid.v4(),
+    title,
+    complete: false
+  }
+  this.setState({ todos: [...this.state.todos, newTodo]})
+}
+
 
 render() {
   return (
     <div className="App">
       <div className="container">
         <Header />
-        <AddTodo />
+        <AddTodo addTodo={this.addTodo} />
         <Todos todos={this.state.todos} markComplete={this.markComplete} delTodo={this.delTodo}/>
       </div>
     </div>
